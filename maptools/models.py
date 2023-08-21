a0ee1df6958ea227a3c86bc790f1bb3fce3b9dd5
gennadynemchin@gmail.com <gennadynemchin@gmail.com>
2023-08-21 12:29:52 +0300

    Add null/blank to lat/lon in PlaceGeoInfo model;

--- maptools/models.py
+++ maptools/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 
 
 class PlaceGeoInfo(models.Model):
-    lat = models.FloatField(verbose_name='широта')
-    lon = models.FloatField(verbose_name='долгота')
+    lat = models.FloatField(verbose_name='широта', null=True, blank=True)
+    lon = models.FloatField(verbose_name='долгота', null=True, blank=True)
     address = models.CharField(max_length=200, unique=True)
     request_date = models.DateTimeField(default=timezone.now)
